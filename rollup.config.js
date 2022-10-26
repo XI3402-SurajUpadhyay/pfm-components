6257071821d4ea5f28f814305788fea8177ac4c2
suraj <suraj.upadhyay@xebia.com>
2022-10-26 17:08:27 +0530

    refactor: changed the rollup version to 2.0.0

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,23 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import babel from "@rollup/plugin-babel";
+import postcss from "rollup-plugin-postcss";
 
 const packageJson = require("./package.json");
 
 export default [
   {
-    // UMD
-    input: packageJson.input,
-    plugins: [
-      resolve(),
-      babel({
-        babelHelpers: "bundled",
-      }),
-    ],
-    output: {
-      file: packageJson.umd,
-      format: "umd",
-      name: "pfmComponentsLibrary", // this is the name of the global object
-      esModule: false,
-      exports: "named",
-      sourcemap: true,
-    },
-  },
-  {
-    // ESM and CJS
+    //UMD, ESM and CJS
     input: packageJson.input,
     output: [
+      {
+        file: packageJson.umd,
+        format: "umd",
+        name: "pfmComponentsLibrary", // this is the name of the global object
+        esModule: false,
+        exports: "named",
+        sourcemap: true,
+      },
       {
         file: packageJson.main,
         format: "cjs",
@@ -38,6 +29,13 @@ export default [
         sourcemap: true,
       },
     ],
-    plugins: [resolve(), commonjs()],
+    plugins: [
+      resolve(),
+      commonjs(),
+      postcss(),
+      babel({
+        babelHelpers: "bundled",
+      }),
+    ],
   },
 ];
