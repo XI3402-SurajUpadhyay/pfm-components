b76a25b63d00f82a7590ad7499d66913893387ce
suraj <suraj.upadhyay@xebia.com>
2022-10-28 16:42:40 +0530

    add: integrated sunburst chart

--- src/components/charts/Sunburst/Sunburst.js
+++ src/components/charts/Sunburst/Sunburst.js
@@ -1,7 +1,31 @@
+import "./index.css";
 
-import './index.css'
-const Sunburst = () => {
-    console.log('Sunburst');
-}
+import { RenderSunburstChart } from '../../../Library/SunburstChart'
 
-export default Sunburst
+const defaultConfig = {
+  data: null,
+  width: 600,
+  targetId: "sunburst-chart",
+};
+
+const Sunburst = (config = defaultConfig) => {
+  if (
+    !config ||
+    typeof config !== "object" ||
+    !config.data ||
+    !config.data.length ||
+    !config.width ||
+    !config.targetId
+  ) {
+    const e ="config structure is not as defined: " + JSON.stringify(defaultConfig)
+    console.log(e);
+    throw new Error(e);
+    return;
+  }
+  
+  console.log("Sunburst");
+  const chart = new RenderSunburstChart(config)
+  chart.renderChart()
+};
+
+export default Sunburst;
